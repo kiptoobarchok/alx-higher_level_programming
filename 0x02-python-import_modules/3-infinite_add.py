3cf68ede79a9d302532809f88d982dc168463ced
kiptoobarchok <calebkiptoo9090@gmail.com>
2023-09-14 09:59:26 +0000

    linter error

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     import sys
 
     t = 0
-    for i in range (len(sys.argv) - 1):
+    for i in range(len(sys.argv) - 1):
         t += int(sys.argv[i + 1])
 
     print("{}".format(t))
