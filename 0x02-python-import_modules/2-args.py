12edad1d7fae6c84ff3979c6dfd342fc73882ca0
kiptoobarchok <calebkiptoo9090@gmail.com>
2023-09-14 07:16:41 +0000

    cmdline args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,13 +2,18 @@
 
 if __name__ == "__main__":
     import sys
-
     count = len(sys.argv) - 1
-    if count == 0:
-        print("0 arguments.")
+
+    if i == 0:
+        print("{} arguments.".format(count))
     elif count == 1:
-        print("1 argument:")
+        print("{} argument:".format(count))
     else:
         print("{} arguments:".format(count))
-    for i in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+
+    if count >= 1:
+        count = 0
+        for arg in sys.argv:
+            if count != 0:
+                print("{}: {}".format(count, arg))
+            count += 1
