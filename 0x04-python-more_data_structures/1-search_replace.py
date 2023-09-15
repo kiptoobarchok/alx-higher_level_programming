2002d1d2037fa774e5e916c3a99f42ee2eb1fc1f
kiptoobarchok <calebkiptoo9090@gmail.com>
2023-09-15 18:11:56 +0000

    more data structures

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def search_replace(my_list, search,replace):
+def search_replace(my_list, search, replace):
     copy = []
     for i in range(len(my_list)):
         if my_list[i] == search:
