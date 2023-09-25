a20e898acb1c12e2cc4b24ac493e306a03a59ece
kiptoobarchok <calebkiptoo9090@gmail.com>
2023-09-25 10:51:51 +0000

    task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,9 +2,9 @@
 
 def safe_print_division(a, b):
     try:
-        div = a / b
-    except (TypeError, ZeroDivisionError):
-        div = None
+        result = a / b
+    except (ZeroDivisionError):
+        result = None
     finally:
-        print("Inside Result: {}".format(div))
-    return (div)
+        print("Inside Result: {}".format(result))
+    return (result)
