da176dcc62bf876df70282afb0bcc15cc390aa13
kiptoobarchok <calebkiptoo9090@gmail.com>
2023-09-26 17:57:07 +0000

    task 2

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 
+"""Define class - square"""
+
 
 class Square:
     """represent square"""
 
     def __init__(self, size):
+        """initialize a new class
+            Args:
+			    size(int): size if new class
+		"""
         self.__size = size
