98eeb1d745d5cecffccefcc7fe04c0c02a910a6a
kiptoobarchok <calebkiptoo9090@gmail.com>
2023-10-02 17:49:59 +0000

    task 3

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -5,7 +5,7 @@ class Rectangle:
 	"""
 		Rectangle class with height and width attributes
 	"""
-	def __init(self, width=0, height=0):
+	def __init__(self, width=0, height=0):
 		"""
 			initialize rectangle instance with
 			width and height
