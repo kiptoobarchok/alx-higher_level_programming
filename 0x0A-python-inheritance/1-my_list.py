bf66ff6c9f5f7b15bb57088777559c3854931b75
kiptoobrachok <calebkiptoo9090@gmail.com>
2023-10-09 21:00:31 +0300

    task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def is_same_class(obj, a_class):
+    "fucntion to check if an object is an instance of given class"
+    if type(obj) == a_class:
+        return True
+    return False
