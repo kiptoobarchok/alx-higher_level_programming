b61476453d6168b4e60872e092ab2042da58fb1d
kiptoobrachok <calebkiptoo9090@gmail.com>
2023-10-09 20:42:16 +0300

    indent error

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+"module"
 
 
 def lookup(obj):
-	"function that lists available attributes and methods of an object"
-	return (dir(obj))
+    "function that lists available attributes and methods of an object"
+    return (dir(obj))
