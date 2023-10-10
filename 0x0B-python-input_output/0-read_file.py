9ea920f3a6cd0f1ddd7381d77fe28f1c0cbc0312
kiptoobrachok <calebkiptoo9090@gmail.com>
2023-10-10 19:51:18 +0300

    input/output python

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     "output contents of file"
-    with open(filename, 'r' encoding="utf-8") as f:
+    with open(filename, 'r' ,encoding="utf-8") as f:
         print(f.read(), end="")
