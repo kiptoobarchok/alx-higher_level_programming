2c9d540c6b58065e1d6e7ed5a5489cd7d0bff5bd
kiptoobrachok <calebkiptoo9090@gmail.com>
2023-10-10 19:47:35 +0300

    input/output python

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"function to read from file"
+
+
+def read_file(filename=""):
+    "output contents of file"
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
