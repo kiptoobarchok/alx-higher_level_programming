dc85440309cf21b0e38c7c24632121809632a306
kiptoobrachok <calebkiptoo9090@gmail.com>
2023-10-11 17:46:38 +0300

    inheritance

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -6,6 +6,6 @@ class MyInt(int):
     "inverted operators"
     def __eq__(self, value):
         return self.real != (value)
-    
+
     def __ne__(self, value):
         return self.real == (value)
