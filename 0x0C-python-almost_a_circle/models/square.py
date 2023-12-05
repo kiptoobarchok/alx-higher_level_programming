b88ee6b253543e14ae157151419e906408fc65df
kiptoobrachok <calebkiptoo9090@gmail.com>
2023-12-05 13:27:34 +0300

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,21 +1,39 @@
 #!/usr/bin/python3
-"class that inherits from Rectangle"
+'''
+    class Square:
+        that inherits from Rectangle
+'''
+
+
 from .rectangle import Rectangle
 
 class Square(Rectangle):
+    '''
+        defining class Square
+        attributes
+    '''
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
         self.width = size
         self.height = size
 
     def __str__(self):
+        '''
+        function to customize a tring representation
+        '''
         return (f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}")
     
     @property
     def size(self):
+        '''
+        function to get size of square
+        '''
         return self.width
 
     @size.setter
     def size(self, value):
+        '''
+        function to set size of square and validate it
+        '''
         self.width = value
         self.height = value
