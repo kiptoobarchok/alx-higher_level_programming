b88402f2da58dcc9ca27263cd05bbd5d06d76077
kiptoobrachok <calebkiptoo9090@gmail.com>
2023-12-05 12:21:49 +0300

    almost a circle doc

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -120,5 +120,4 @@ class Rectangle(Base):
 
     def __str__(self):
         "function to return a customized string"
-        return (f"[Rectangle] ({self.id})\
-                {self.__x}/{self.__y} - {self.__width}/{self.__height}")
+        return (f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}")
