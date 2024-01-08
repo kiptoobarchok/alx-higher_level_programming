38631fb30712f7dc9d1d6eae3fec303202a9755a
kiptoobrachok <calebkiptoo9090@gmail.com>
2024-01-08 22:30:08 +0300

    loops

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const arg = process.argv[2];
+
+const size = parseInt(arg, 10);
+
+if (isNaN(size) || size <= 0) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    let row = '';
+    for (let j = 0; j < size; j++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
+}
