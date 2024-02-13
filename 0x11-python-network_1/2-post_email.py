155a39a266c6b010e650d9c7bb88659e258ae1ff
kiptoobrachok <calebkiptoo9090@gmail.com>
2024-02-14 00:23:53 +0300

    task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+send POST request to url with email parameter
+"""
+
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        url = sys.argv[1]
+        email = sys.argv[2]
+
+        data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+        request = urllib.request.Request(url, data=data, method='POST')
+
+        with urllib.request.urlopen(request) as response:
+            body = response.read().decode('utf-8')
+            print(body)
+
+    else:
+        print("Usage: {} <URL> <email>".format(sys.argv[0]))
+        sys.exit(1)
