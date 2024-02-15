bcd08c379006cacb6585e27ada0a9f324d0e0e89
kiptoobrachok <calebkiptoo9090@gmail.com>
2024-02-15 14:43:31 +0300

    task 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"Python script that fetches: https://alx-intranet.hbtn.io/status"
+
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+
+    response = requests.get(url)
+
+    print("Body response:")
+    print("\t- type:", type(response.text))
+    print("\t- content:", response.text)
